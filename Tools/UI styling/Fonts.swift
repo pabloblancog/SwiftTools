a054cf116ad481f4742ebea5b0cc4522f9d859b5
Pablo Blanco González <pabloblancogonzalez@gmail.com>
2019-02-17 17:54:33 +0100

    Improve font creators

--- Tools/UI styling/Fonts.swift
+++ Tools/UI styling/Fonts.swift
@@ -24,29 +24,29 @@ struct Fonts {
     fileprivate static let defaultFontSize: CGFloat = 14.0
 
     // Headings
-    static let heading1: UIFont       = UIFont.font(size: 30, type: .bold, family: Fonts.FontFamily.primary.rawValue)
-    static let heading2: UIFont       = UIFont.font(size: 22, type: .light, family: Fonts.FontFamily.primary.rawValue)
-    static let heading3: UIFont       = UIFont.font(size: 16, type: .medium, family: Fonts.FontFamily.primary.rawValue)
-    static let heading4: UIFont       = UIFont.font(size: 16, type: .light, family: Fonts.FontFamily.primary.rawValue)
-    static let heading5: UIFont       = UIFont.font(size: 14, type: .medium, family: Fonts.FontFamily.primary.rawValue)
-    static let heading6: UIFont       = UIFont.font(size: 14, type: .light, family: Fonts.FontFamily.primary.rawValue)
+    static let heading1: UIFont       = UIFont.font(size: 30, type: .bold)
+    static let heading2: UIFont       = UIFont.font(size: 22, type: .light)
+    static let heading3: UIFont       = UIFont.font(size: 16, type: .medium)
+    static let heading4: UIFont       = UIFont.font(size: 16, type: .light)
+    static let heading5: UIFont       = UIFont.font(size: 14, type: .medium)
+    static let heading6: UIFont       = UIFont.font(size: 14, type: .light, family: .secondary)
     
     // Body
-    static let body1: UIFont       = UIFont.font(size: 16, type: .medium, family: Fonts.FontFamily.primary.rawValue)
-    static let body2: UIFont       = UIFont.font(size: 14, type: .light, family: Fonts.FontFamily.primary.rawValue)
+    static let body1: UIFont       = UIFont.font(size: 16, type: .medium)
+    static let body2: UIFont       = UIFont.font(size: 14, type: .light)
 
     // Captions
-    static let caption1: UIFont       = UIFont.font(size: 14, type: .light, family: Fonts.FontFamily.primary.rawValue)
+    static let caption1: UIFont       = UIFont.font(size: 14, type: .light)
 
     // Labels
-    static let label1: UIFont       = UIFont.font(size: 14, type: .light, family: Fonts.FontFamily.primary.rawValue)
+    static let label1: UIFont       = UIFont.font(size: 14, type: .light)
 
     // Links
-    static let link: UIFont       = UIFont.font(size: 16, type: .medium, family: Fonts.FontFamily.primary.rawValue)
+    static let link: UIFont       = UIFont.font(size: 16, type: .medium)
     
     // Buttons
-    static let button1: UIFont       = UIFont.font(size: 20, type: .bold, family: Fonts.FontFamily.primary.rawValue)
-    static let button2: UIFont       = UIFont.font(size: 20, type: .bold, family: Fonts.FontFamily.primary.rawValue)
+    static let button1: UIFont       = UIFont.font(size: 20, type: .bold)
+    static let button2: UIFont       = UIFont.font(size: 20, type: .bold)
     
     // Default
     static let defaultFont: UIFont      = UIFont.systemFont(ofSize: defaultFontSize)
@@ -56,8 +56,8 @@ extension UIFont {
 
     class func font(size fontSize: CGFloat,
                     type fontFamilyType: Fonts.FontType,
-                    family fontFamilyName: String) -> UIFont {
-        let name = "\(fontFamilyName)-\(fontFamilyType.rawValue)"
+                    family fontFamilyName: Fonts.FontFamily = .primary) -> UIFont {
+        let name = "\(fontFamilyName.rawValue)-\(fontFamilyType.rawValue)"
         guard let font = UIFont(name: name, size: fontSize) else {
             return Fonts.defaultFont
         }
